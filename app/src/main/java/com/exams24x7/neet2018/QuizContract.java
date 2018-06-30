0afc66a2038834047d3adef83db55024dead1033
ashokshenoy66 <ashokshenoy66@gmail.com>
2018-06-30 13:53:57 +0530

    Uptodate with persistant scores, color coding for responses etc

--- app/src/main/java/com/exams24x7/neet2018/QuizContract.java
+++ app/src/main/java/com/exams24x7/neet2018/QuizContract.java
@@ -2,11 +2,11 @@ package com.exams24x7.neet2018;
 
 import android.provider.BaseColumns;
 
-public class QuizContract {
+public final class QuizContract {
 
     private QuizContract() {}
 
-    public static class QuestionsTable implements BaseColumns {
+    public static class QuestionsTable implements BaseColumns{
         public static final String TABLE_NAME = "quiz_questions";
         public static final String COLUMN_QUESTION = "question";
         public static final String COLUMN_OPTION1 = "option1";
